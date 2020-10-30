89030ab47333e41a8e4a6bd05edd3f2be19cf3f4
LIANGYU <n795113@gmail.com>
2020-10-30 22:06:09 +0800

    Add index-view page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container mx-auto px-4">
+        <h2 class="text-blue-500 uppercase tracking-wide font-semibold">
+            Popular Games
+        </h2>
+        <div class="popular-games text-sm grid grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="/images/zelda-btw.jpg"
+                            alt="game cover"
+                            class="hover:opacity-75 transition ease-in-out duration-150"
+                        >
+                    </a>
+                    <div class="absolute b0ttom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+                        style="right: -20px; bottom:-20px"
+                    >
+                        <div class="text-sm font-semiblod flex items-center justify-center h-full">90%</div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                    Zela: Breath of Wild
+                </a>
+            <div class="text-gray-400 mt-1">Nitendo Switch</div>
+            </div>
+        </div> <!-- end popular-games -->
+        <div class="flex my-10">
+            <div class="recently-reviewed w-3/4 mr-32">
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
+                <div class="recently-reviewed-container space-y-12 mt-8">
+                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="/images/zelda-btw.jpg"
+                                    alt="game cover"
+                                    class="w-48 hover:opacity-75 transition ease-in-out duration-150"
+                                >
+                            </a>
+                            <div class="absolute b0ttom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
+                                style="right: -20px; bottom:-20px"
+                            >
+                                <div class="text-sm font-semiblod flex items-center justify-center h-full">90%</div>
+                            </div>
+                        </div> <!-- end relative -->    
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+                                Zela: Breath of Wild
+                            </a>
+                            <div class="text-gray-400 mt-1">Nitendo Switch</div>
+                            <p class="mt-6 text-gray-400">
+                            Step into a world of discovery, exploration and adventure in The Legend of Zelda: Breath of the Wild, a boundary-breaking new game in the acclaimed series. Travel across fields, through forests and to mountain peaks as you discover what has become of the ruined kingdom of Hyrule in this stunning open-air adventure.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="most-anticipate w-1/4">
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most Anticipated</h2>
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="/images/zelda-btw.jpg"
+                                alt="game cover"
+                                class="w-16 hover:opacity-75 transition ease-in-out duration-150"
+                            >    
+                        </a>
+                        <div class="ml-4">
+                            <a href="#">Zelda: Breath of Wild</a>
+                            <div class="text-gray-400 text-sm mt-1">Sep 16, 2020</div>
+                        </div>    
+                    </div> 
+                </div> <!-- end most-anticipated -->
+
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold mt-6">Coming Soon</h2>
+                <div class="coming-soon-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="/images/zelda-btw.jpg"
+                                alt="game cover"
+                                class="w-16 hover:opacity-75 transition ease-in-out duration-150"
+                            >    
+                        </a>
+                        <div class="ml-4">
+                            <a href="#">Zelda: Breath of Wild</a>
+                            <div class="text-gray-400 text-sm mt-1">Sep 16, 2020</div>
+                        </div>    
+                    </div> 
+                </div> <!-- end coming-soon -->
+            </div>
+        </div>
+    </div> <!-- end container -->
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Video Games</title>
+    
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto items-center flex justify-between px-4 px-6">
+            <div class="flex items-center">
+                <a href="/" class="font-semibold">
+                    Home
+                </a>
+                <ul class="flex ml-16 space-x-8">
+                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+                    <li><a href="#"  class="hover:text-gray-400">Coming Soon</a></li>
+                </ul>
+            </div>
+            <div class="flex items-center">
+                <div class="relative">
+                    <input type="text"class="bg-gray-800 text-sm rounded-full w-64 px-3 pl-8 py-1
+                    focus:outline-none foucus:shadow-outline"
+                    placeholder="search...">
+                    <div class="absolute top-0 flex items-center h-full ml-2">
+                        <svg class="w-4 fill-current text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                    </div>    
+                </div>
+                <div class="ml-6">
+                    <a href="#"><img src="https://avatars.dicebear.com/api/male/.svg"
+                    alt="avatar" class="rounded-full w-8 bg-white"></a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main class="py-8">
+        @yield('content')
+    </main>
+
+    <footer class="border-t border-gray-800">
+        <div class="container mx-auto px-4 py-6">
+            Powered By <a href="#" class="underline hover:text-gray-400">IGDB API</a>    
+        </div>
+        
+    </footer>
+</body>
+</html>
