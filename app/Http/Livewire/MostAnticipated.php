bcf5d1cc9916b8b69c036e7c3a6d49836aea6f24
LIANGYU <n795113@gmail.com>
2020-11-05 15:05:11 +0800

    refactor: Move logics into ctl for mostAnticipated
    - Move formating logic from view into controller

--- app/Http/Livewire/MostAnticipated.php
+++ app/Http/Livewire/MostAnticipated.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http as Http;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 
 class MostAnticipated extends Component
 {
@@ -16,7 +17,7 @@ class MostAnticipated extends Component
         $after = Carbon::now()->addMonths(6)->timestamp;
         $current = Carbon::now()->timestamp;
 
-        $this->mostAnticipated = Cache::remember('most-anticipated', 7, function () use($current, $after){
+        $unformat = Cache::remember('most-anticipated', 7, function () use($current, $after){
             return Http::withHeaders(config('services.igdb'))->withBody("
                 fields name,first_release_date,cover.url, slug;
                 sort rating desc;
@@ -24,13 +25,23 @@ class MostAnticipated extends Component
                 limit 4;
             ",'raw')->post('https://api.igdb.com/v4/games')->json();
         });
-
-        //dd($this->mostAnticipated);
-        //dd('after '.$after.'   current '.$current.'   release '.$this->mostAnticipated[1]['first_release_date']);
+        
+        $this->mostAnticipated = $this->formatForView($unformat);
     }
 
     public function render()
     {
         return view('livewire.most-anticipated',['mostAnticipated' => $this->mostAnticipated]);
+    }
+
+    public function formatForView($games)
+    {
+        return collect($games)->map(function($game){
+            return collect($game)->merge([
+                'coverImageUrl' => $game['cover']?Str::replaceFirst('thumb','cover_small',$game['cover']['url']):null,
+                'link' => route('games.show', $game['slug']),
+                'release_date' => Carbon::parse($game['first_release_date'])->format('M d, Y'),
+            ]);
+        })->toArray();
     }
 }
--- resources/views/livewire/most-anticipated.blade.php
+++ resources/views/livewire/most-anticipated.blade.php
@@ -2,16 +2,16 @@
 @forelse($mostAnticipated as $game)
     <div class="most-anticipated-container space-y-10 mt-8">
         <div class="game flex">
-            <a href="{{ route('games.show', $game['slug']) }}">
-                <img src="{{ Str::replaceFirst('thumb','cover_small',$game['cover']['url']) }}"
+            <a href="{{ $game['release_date'] }}">
+                <img src="{{ $game['coverImageUrl'] }}"
                     alt="game cover"
                     class="w-16 hover:opacity-75 transition ease-in-out duration-150"
                 >    
             </a>
             <div class="ml-4">
-                <a href="{{ route('games.show', $game['slug']) }}">{{$game['name']}}</a>
+                <a href="{{ $game['link'] }}">{{$game['name']}}</a>
                 <div class="text-gray-400 text-sm mt-1">
-                    {{ Carbon\Carbon::parse($game['first_release_date'])->format('M d, Y') }}
+                    {{ $game['release_date'] }}
                 </div>
             </div>    
         </div> 
