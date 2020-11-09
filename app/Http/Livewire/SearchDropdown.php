74b4c8e862116921c8b208ae2b93077927c0bb7e
LIANGYU <n795113@gmail.com>
2020-11-09 14:53:47 +0800

    feat: Add search bar
    issue: #

---
+++ app/Http/Livewire/SearchDropdown.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use Illuminate\Support\Facades\Http as Http;
+use Illuminate\Support\Str;
+
+class SearchDropdown extends Component
+{
+    public $search = '';
+    public $searchResults = [];
+
+    public function render()
+    {
+        if(strlen($this->search) < 2){
+            return view('livewire.search-dropdown');
+        }else{
+            $unformatGames = Http::withHeaders(config('services.igdb'))->withBody("
+                    fields name,cover.url,slug;
+                    search \"{$this->search}\";
+                    
+                    limit 8;
+            ",'raw')->post('https://api.igdb.com/v4/games')->json();
+
+
+            $this->searchResults = $this->formatForView($unformatGames);
+            
+            
+            return view('livewire.search-dropdown');    
+        }
+    }
+
+    private function formatForView($games)
+    {
+        return collect($games)->map(function($game){
+            return collect($game)->merge([
+                'coverImageUrl' => isset($game['cover'])?Str::replaceFirst('thumb','cover_small',$game['cover']['url']):null,
+                'rating' => isset($game['rating'])?round($game['rating']):null,
+                'link' => isset($game['slug'])?route('games.show', $game['slug']):null,
+            ]);
+        })->toArray();
+    }
+}
---
+++ resources/views/livewire/search-dropdown.blade.php
@@ -0,0 +1,36 @@
+<div class="relative">
+    <input
+        wire:model.debounce.300ms="search" 
+        type="text"
+        class="bg-gray-800 text-sm rounded-full w-64 px-3 pl-8 py-1
+            focus:outline-none foucus:shadow-outline"
+        placeholder="search..."
+    >
+
+    <div class="absolute top-0 flex items-center h-full ml-2">
+        <svg class="w-4 fill-current text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
+        </svg>
+    </div>    
+
+    <div wire:loading class="spinner right-0 top-0 mt-3 mr-4" style="position:absolute"></div>
+
+    @if(strlen($search)>=2)
+    <div class="absolute z-50 bg-gray-800 text-xs rounded w-64 mt-2">
+        <ul>
+            @if(count($searchResults)>0)
+                @foreach($searchResults as $game)
+                    <li class="border-b border-gray-700">
+                        <a href="{{$game['link']}}" class="block hover:bg-gray-700 px-3 py-3 flex items-center transition ease-in-out duration-150">
+                            <img src="{{$game['coverImageUrl']}}" alt="game cover" class="w-10">
+                            <span class="ml-4">{{$game['name']}}</span>
+                        </a>
+                    </li>
+                @endforeach
+            @else
+                No results for "{{$search}}"
+            @endif
+        </ul>
+    </div>
+    @endif
+</div>
